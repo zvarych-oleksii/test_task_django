22253e1c8ecf204b8ee209e8456a1fb7263bbb06
oleksii-zvarych <oleksii.zvarych@lnu.edu.ua>
2024-01-04 17:13:34 +0200

    Added docker

--- email_validation/admin.py
+++ email_validation/admin.py
@@ -7,14 +7,4 @@ from django.contrib import admin
 
 from email_validation.models import EmailVerification
 
-
-@admin.site.register(EmailVerification)
-class EmailVerificationAdmin(admin.ModelAdmin):
-    """
-    Admin class for EmailVerification model.
-
-    This class defines the display configuration for the EmailVerification model
-    in the Django admin interface.
-    """
-
-    list_display = ('email', 'timestamp')
+admin.site.register(EmailVerification)
